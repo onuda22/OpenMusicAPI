f02af3d6d478b9a662af8116e70283d5cdaf05de
onuda22 <okinabada@gmail.com>
2025-02-03 23:03:42 +0700

    feat(validator): implement data validator payload for songs model

--- src/validator/songs/index.js
+++ src/validator/songs/index.js
@@ -1,12 +1,13 @@
-const Joi = require('joi');
+const InvariantError = require('../../exceptions/InvariantError');
+const { SongPayloadSchema } = require('./schema');
 
-const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
-  year: Joi.number().required(),
-  genre: Joi.string().required(),
-  performer: Joi.string().required(),
-  duration: Joi.number().optional(),
-  albumId: Joi.string().optional(),
-});
+const SongsValidator = {
+  validateSongPayload: (payload) => {
+    const validationResult = SongPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
 
-module.exports = SongPayloadSchema;
+module.exports = SongsValidator;
