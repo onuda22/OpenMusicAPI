3d04987beb842fb107e7711d764ab6f5e78df087
onuda22 <okinabada@gmail.com>
2025-02-03 22:35:51 +0700

    feat(migration): migrate create albums table

--- migrations/1738580729042_create-table-albums-migrations.js
+++ migrations/1738580729042_create-table-albums-migrations.js
@@ -8,11 +8,28 @@ exports.shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+  });
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
