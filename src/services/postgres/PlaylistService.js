5770f1dd881b19898791908e76eab1a39178798f
onuda22 <okinabada@gmail.com>
2025-03-07 20:20:24 +0700

    feat(service): Add Service for CRD songs on playlists

--- src/services/postgres/PlaylistService.js
+++ src/services/postgres/PlaylistService.js
@@ -80,6 +80,76 @@ class PlaylistService {
       throw new AuthorizationError('You are forbidden to access this resource');
     }
   }
+
+  async getPlaylistById(id) {
+    const query = {
+      text: `SELECT playlists.id, playlists.name, users.username
+      FROM playlists
+      LEFT JOIN users ON users.id = playlists.owner
+      WHERE playlists.id = $1`,
+      values: [id],
+    };
+
+    return (await this._pool.query(query)).rows[0];
+  }
+
+  // Playlist Songs Feature
+  /**
+   * TODO:
+   * uri: playlists/{id}/songs
+   * 1. POST song (songId) return id
+   * 2. GET song playlists return []
+   * 3. DELETE song (songId) return void
+   * Auth required
+   */
+
+  //POST
+  async addPlaylistSong(playlistId, songId) {
+    const id = `ps-${nanoid(16)}`;
+    const createdAt = new Date().toISOString();
+    const query = {
+      text: 'INSERT INTO playlist_songs VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, playlistId, songId, createdAt, createdAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Song failed to be added to playlists');
+    }
+  }
+
+  //GET ALL
+  async getAllPlaylistSong(playlistId) {
+    const playlistData = await this.getPlaylistById(playlistId);
+
+    const query = {
+      text: `SELECT songs.id, songs.title, songs.performer
+		          FROM playlist_songs
+		          RIGHT JOIN songs ON songs.id = playlist_songs.song_id
+		          WHERE playlist_songs.playlist_id = $1`,
+      values: [playlistId],
+    };
+
+    const songsData = await this._pool.query(query);
+    playlistData.songs = songsData.rows;
+
+    return playlistData;
+  }
+
+  //DELETE
+  async deleteSongFromPlaylistById(playlistId, songId) {
+    const query = {
+      text: 'DELETE FROM playlist_songs WHERE playlist_id = $1 AND song_id = $2',
+      values: [playlistId, songId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Song Not Found');
+    }
+  }
 }
 
 module.exports = PlaylistService;
